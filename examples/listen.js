ce3580d2fa05d4018853bbd3783501f039c14815
rubyrubenstahl <ruby@ruby-lighting.com>
2019-07-20 20:39:19 -0400

    Adds new events to listening sample

--- examples/listen.js
+++ examples/listen.js
@@ -4,3 +4,19 @@ const ups = new UPS();
 ups.on('change', ({ property, value }) =>
     console.log(`The ${property} value has changed to ${value}`)
 )
+
+ups.on('connected', deviceDescriptor => {
+    console.log('Connected')
+    console.log(deviceDescriptor)
+})
+
+ups.on('disconnected', deviceDescriptor => {
+    console.log('Disconnected')
+    console.log(deviceDescriptor)
+})
+
+ups.on('initialized', state => {
+    console.log('Initialized')
+    console.log(state)
+})
+
